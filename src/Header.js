e45db56ea8be374f40a7d6d3f92a084e0932a70c
stephanie <stephaniechibueze84@gmail.com>
2024-07-23 22:46:32 +0100

    laststickynotes

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Sticky Notes</h1>
+      <img src="./images/noteimg.png" className="App-Img" alt="stickynotes" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/notes.js
@@ -0,0 +1,15 @@
+const notes = [
+    {
+        Key: 1,
+        title: "List of Courses",
+        Content: "UI/UX Designer, Front-end Software Dev ETC."
+    },
+
+    {
+        key: 2,
+        title: "Front-end course",
+        Content:"CSS, HTML, JAVASCRIPT ETC."
+    },
+];
+
+export default notes;
